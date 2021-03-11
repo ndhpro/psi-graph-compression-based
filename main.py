cc0df395a2fc16c7655a4fa84ee78115bbe920e6
ndhpro <ndhpro.psa@gmail.com>
2021-03-11 21:35:30 +0700

    *

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import networkx as nx
 import pandas as pd
 from glob import glob
-from joblib import dump
+from joblib import dump, load
 from utils import load_graph
 from karateclub import Graph2Vec
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Normalizer, StandardScaler
 from imblearn.over_sampling import SMOTE
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+paths = glob('data/patterns/*/*.txt')
 
 
 def largest_cc(psi_path):
@@ -18,17 +22,20 @@ def largest_cc(psi_path):
 
 
 print('Load graphs...')
-paths = glob('data/patterns/*/*.txt')
 graphs = []
-labels = [0 if 'benign' in path else 0 for path in paths]
+labels = []
 for path in paths:
     graph = nx.read_edgelist(path)
-    if len(graph) < 5:
+    if len(graph) < 10:
         psi_path = path.replace('patterns', 'psi_graph')
         graph = largest_cc(psi_path)
     mapping = dict(zip(graph, range(len(graph))))
     graph = nx.relabel_nodes(graph, mapping)
     graphs.append(graph)
+    if 'benign' in path:
+        labels.append(0)
+    else:
+        labels.append(1)
 
 print('Performing Graph2vec...')
 graph2vec = Graph2Vec()
@@ -37,3 +44,24 @@ graphs = graph2vec.get_embedding()
 
 dump(graphs, 'data/graphs.joblib')
 dump(labels, 'data/labels.joblib')
+graphs = load('data/graphs.joblib')
+labels = load('data/labels.joblib')
+
+print(len(graphs), len(labels))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    graphs, labels, test_size=.3, random_state=42
+)
+
+smote = SMOTE()
+smote.fit_resample(X_train, y_train)
+
+normalizer = StandardScaler()
+X_train = normalizer.fit_transform(X_train, y_train)
+X_test = normalizer.transform(X_test)
+
+estimator = RandomForestClassifier(n_jobs=-1)
+estimator.fit(X_train, y_train)
+y_pred = estimator.predict(X_test)
+report = classification_report(y_test, y_pred, digits=4)
+print(report)
